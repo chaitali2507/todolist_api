e6a9386809c23d57220ba32ab939f5a5e7edb2d6
chaitali2507 <chaitalirathod304@gmail.com>
2024-05-22 11:26:47 +0530

    todolist_api

---
+++ controller/todolist_controller.js
@@ -0,0 +1,32 @@
+var todolist= require('../model/todolist_model');
+exports.insert = async (req,res) =>{
+     var data= await todolist.create(req.body);
+     res.status(200).json({
+        status:"data insert",
+        data
+     })
+}
+exports.get_data = async (req,res) =>{
+    var data= await todolist.find(req.body);
+    res.status(200).json({
+       status:"data select",
+       data
+    })
+}
+exports.delete_data=async (req,res)=>{
+    var id= req.params.id;
+    var data= await todolist.findByIdAndDelete(id);
+
+    res.status(200).json({
+        status:"data delete",
+    })
+ } 
+ exports.update_data=async (req,res)=>{
+    var id= req.params.id;
+    var data= await todolist.findByIdAndUpdate(id,req.body);
+
+    res.status(200).json({
+        status:"data updated",
+        data
+    })
+ } 
